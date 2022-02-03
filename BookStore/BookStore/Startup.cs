8e3af138bdcfab2d3dd21f635519b9aba5ada150
Christian Afreh <afrehchristian@gmail.com>
2022-02-03 22:57:55 +0000

    Added book details

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -17,18 +17,15 @@ namespace BookStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-#if DEBUG
-            services.AddRazorPages().AddRazorRuntimeCompilation();
-#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            { 
+            {
                 app.UseDeveloperExceptionPage();
-            } 
+            }
             app.UseStaticFiles();
 
             app.UseRouting();
